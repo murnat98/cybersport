ae51b8275a987b5e3217c87077255717a944d161
murnat98 <murnat98@yandex.ru>
2018-01-28 16:46:41 +0300

    print messages added to shell scripts

--- shell-scripts/backend/deploy.sh
+++ shell-scripts/backend/deploy.sh
@@ -6,19 +6,24 @@ return_dir=$(pwd)
 if [[ $1 == "create" ]] # create for first time
 then
 	# Clone code from repository
+    echo "Cloning repository..."
 	git clone https://github.com/murnat98/cybersport.git ${project_dir}
 
 	# Create virtual environment
+	echo "Creating virtual environment..."
 	python3.6 -m venv ${project_dir}/env
 else
 	cd ${project_dir}
 
+    echo "Pull changes from repository..."
 	git pull
+	echo "Migrating changes to database..."
     ./src/manage.sh migrate
 
     cd ${return_dir}
 fi
 
 # install requirements
+echo "Installing project requirements..."
 . ${project_dir}/env/bin/activate
 pip3.6 install -r ${project_dir}/requirements.txt
--- src/manage.sh
+++ src/manage.sh
@@ -2,13 +2,17 @@
 
 project_dir=~/cybersport
 
+echo "Activating virtual environment..."
 . ${project_dir}/env/bin/activate
 
+echo "Executing manage.py..."
 sudo python ${project_dir}/src/manage.py $@
 
 if [[ $1 == "migrate" ]]
 then
+    echo "Synchronizing the slave database..."
     ssh -J 95.163.251.121 student13@192.168.1.25 ~/shell/synchronize-slave.sh
 fi
 
+echo "Deactivating the virtual environment..."
 deactivate
