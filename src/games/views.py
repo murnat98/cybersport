d9693bc2b2854f683be110c717fd9566ca275cc2
murnat98 <murnat98@yandex.ru>
2018-02-01 12:55:17 +0300

    feature: games pagination

--- src/games/views.py
+++ src/games/views.py
@@ -1,9 +1,19 @@
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
 from games.models import Games
 from games.serializers import GamesListSerializer
 
 
+class DefaultPagination(PageNumberPagination):
+    page_size = 10
+
+
+class GamesPagination(DefaultPagination):
+    pass
+
+
 class GamesListView(viewsets.ModelViewSet):
     queryset = Games.objects.all()
     serializer_class = GamesListSerializer
+    pagination_class = GamesPagination
