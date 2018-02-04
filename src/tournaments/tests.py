760bd71c3238c66e6743f1e838402bf823741db5
murnat98 <murnat98@yandex.ru>
2018-02-04 13:52:33 +0300

    overrode functions such as save and create (Queryset)

--- src/tournaments/tests.py
+++ src/tournaments/tests.py
@@ -1,1 +1,18 @@
-# Create your tests here.
+from django.test import TestCase
+
+from tournaments.models import Tournaments
+
+
+class TestTournamentShard(TestCase):
+    names = ('tournament', 'a tournament', '1 t', 'some tournament',)
+
+    def setUp(self):
+        self.created_tournaments = []
+        for name in self.names:
+            self.created_tournaments.append(Tournaments.objects.create(name=name))
+
+    # TODO: write tests
+
+    def tearDown(self):
+        for created_tournament in self.created_tournaments:
+            created_tournament.delete()
--- src/tournaments/views.py
+++ src/tournaments/views.py
@@ -7,3 +7,10 @@ class TournamentsView(DefaultAPIView):
     model = Tournaments
     list_serializer_class = TournamentsListSerializer
     detail_serializer_class = TournamentDetailSerializer
+
+    def get_queryset(self):
+        # TODO: sort it before returning the list
+        # first_queryset = [obj for obj in super().get_queryset()]
+        # second_queryset = [obj for obj in first_queryset.using('shard_2')]
+
+        return [obj for obj in super().get_queryset()] + [obj for obj in super().get_queryset().using('shard_2')]
