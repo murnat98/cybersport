4b5194384e4331123ab3c0c9eaf77f239ddcb10f
murnat98 <murnat98@yandex.ru>
2018-02-05 13:49:23 +0300

    django-jenkins settings

--- src/cybersport/settings.py
+++ src/cybersport/settings.py
@@ -194,6 +194,4 @@ JENKINS_TASKS = (
     'django_jenkins.tasks.run_pylint',
     'django_jenkins.tasks.run_pep8',
     'django_jenkins.tasks.run_pyflakes',
-    'django_jenkins.tasks.with_coverage',
-    'django_jenkins.tasks.django_tests',
 )
